equalsraf/vim-qt

commit 5a101ab08ee863f40e961ae3c99cda39466ece76

Refactor event handling, as delayed events
- Removed loop guard mutex
- Removed most references to gui_mch_update
- Created VimWrapper, a GUI event deferal class
- Switched most Vim method calls to VimWrapper calls
- Merge VimGui and VimWrapper classes
- Move string conversion methods to VimWrapper

src/qt/vimwrapper.cpp
@@ -1,0 +1,346 @@
+#include "vimwrapper.moc"
+
+extern "C" {
+#include "vim.h"
+}
+
+Q_DECLARE_METATYPE( QList<QUrl> );
+
+VimWrapper::VimWrapper(QObject *parent)
+:QObject(parent)
+{
+	qRegisterMetaType<QList<QUrl> >("URLList");
+
+}
+
+void VimWrapper::guiResizeShell(int w, int h)
+{
+	QMetaObject::invokeMethod(this, 
+				"slot_guiResizeShell", Qt::QueuedConnection, 
+				Q_ARG(int, w), 
+				Q_ARG(int, h)
+			);
+	
+}
+void VimWrapper::slot_guiResizeShell(int w, int h)
+{
+	gui_resize_shell(w,h);
+}
+
+void VimWrapper::guiShellClosed()
+{
+	QMetaObject::invokeMethod(this, 
+				"slot_guiShellClosed", 
+				Qt::QueuedConnection);
+}
+void VimWrapper::slot_guiShellClosed()
+{
+	gui_shell_closed();
+}
+
+void VimWrapper::guiSendMouseEvent(int button, int x, int y, int repeated_click, unsigned int modifiers)
+{
+	QMetaObject::invokeMethod(this, 
+				"slot_guiSendMouseEvent", Qt::QueuedConnection, 
+				Q_ARG(int, button), 
+				Q_ARG(int, x), 
+				Q_ARG(int, y), 
+				Q_ARG(int, repeated_click), 
+				Q_ARG(unsigned int, modifiers)
+				);
+}
+void VimWrapper::slot_guiSendMouseEvent(int button, int x, int y, int repeated_click, unsigned int modifiers)
+{
+	gui_send_mouse_event(button, x, y, repeated_click, modifiers);
+}
+
+void VimWrapper::guiMouseMoved(int x, int y)
+{
+	QMetaObject::invokeMethod(this, 
+				"slot_guiMouseMoved",  Qt::QueuedConnection, 
+				Q_ARG(int, x),
+				Q_ARG(int, y)
+				);
+
+}
+void VimWrapper::slot_guiMouseMoved(int x, int y)
+{
+	gui_mouse_moved(x, y);
+}
+
+void VimWrapper::guiFocusChanged(int focus)
+{
+	QMetaObject::invokeMethod(this, 
+				"slot_guiFocusChanged", 
+				Qt::QueuedConnection, Q_ARG(int, focus));
+}
+void VimWrapper::slot_guiFocusChanged(int focus)
+{
+	gui_focus_change(focus);
+}
+
+void VimWrapper::sendTablineEvent(int ev)
+{
+	QMetaObject::invokeMethod(this, 
+				"slot_sendTablineEvent", 
+				Qt::QueuedConnection, Q_ARG(int, ev));
+}
+void VimWrapper::slot_sendTablineEvent(int ev)
+{
+	send_tabline_event(ev);
+}
+
+void VimWrapper::sendTablineMenuEvent(int idx, int ev)
+{
+	QMetaObject::invokeMethod(this, 
+				"slot_sendTablineMenuEvent", 
+				Qt::QueuedConnection, Q_ARG(int, ev),
+							Q_ARG(int, ev));
+}
+void VimWrapper::slot_sendTablineMenuEvent(int idx, int ev)
+{
+	send_tabline_menu_event(idx, ev);
+}
+
+
+void VimWrapper::guiHandleDrop(int x, int y, unsigned int mod, const QList<QUrl> urls)
+{
+	QMetaObject::invokeMethod(this, 
+				"slot_guiHandleDrop", 
+				Qt::QueuedConnection, 
+				Q_ARG(int, x),
+				Q_ARG(int, y),
+				Q_ARG(unsigned int, mod),
+				Q_ARG(QList<QUrl>, urls)
+				);
+}
+void VimWrapper::slot_guiHandleDrop(int x, int y, unsigned int mod, const QList<QUrl> urls)
+{
+	char_u **fnames = (char_u**)alloc( urls.size() * sizeof(char_u*));
+	int i;
+	for (i=0; i<urls.size(); i++) {
+		QByteArray encoded = convertTo(urls.at(i).toString());
+		char *s = (char*)alloc(encoded.size()*sizeof(char)+1);
+		int j;
+		for (j=0; j<encoded.size(); j++) {
+			s[j] = encoded.at(j);
+		}
+		s[j]='\0';
+
+		fnames[i] = (char_u *) s;
+	}
+	gui_handle_drop(x, y, 0, fnames, urls.size());
+}
+
+
+QPoint VimWrapper::mapText(int row, int col) 
+{ 
+	return QPoint( gui.char_width*col, gui.char_height*row );
+}
+
+QPoint VimWrapper::cursorPosition() 
+{ 
+	return mapText(gui.cursor_row, gui.cursor_col);
+}
+
+QRect VimWrapper::mapBlock(int row1, int col1, int row2, int col2)
+{
+	QPoint tl = mapText( row1, col1 );
+	QPoint br = mapText( row2+1, col2+1);
+	br.setX( br.x()-1 );
+	br.setY( br.y()-1 );
+
+	return QRect(tl, br);
+}
+
+QColor VimWrapper::backgroundColor()
+{
+	return fromColor(gui.back_pixel);
+}
+
+QColor VimWrapper::normalColor()
+{
+	return fromColor(gui.norm_pixel);
+}
+
+int VimWrapper::charWidth()
+{
+	return gui.char_width;
+}
+
+int VimWrapper::charHeight()
+{
+	return gui.char_height;
+}
+
+QFont VimWrapper::normalFont()
+{
+	if ( gui.norm_font ) {
+		return *(gui.norm_font);
+	}
+
+	return QFont();
+}
+
+int VimWrapper::charCellWidth(const QChar& c)
+{
+	int len = utf_char2cells(c.unicode());
+	if ( len <= 2 ) {
+		return len;
+	}
+
+	return 0;
+}
+
+int VimWrapper::stringCellWidth(const QString& s)
+{
+	/*
+	 * Vim kindly provides us with utf_char2cells,
+	 * unfortunately Qt does not have a way measure
+	 * wide char length.
+	 */
+	int len=0;
+	foreach ( QChar c, s ) {
+		len += charCellWidth(c);
+	}
+	return len;
+}
+
+QIcon VimWrapper::iconFromTheme(const QString& name)
+{
+	QIcon icon;
+
+	// Theme icons
+	if ( "Open" == name ) {
+		icon = QIcon::fromTheme("document-open", 
+				QIcon(":/icons/document-open.png"));
+	} else if ( "Save" == name ) {
+		icon = QIcon::fromTheme("document-save",
+				QIcon(":/icons/document-save.png"));
+	} else if ( "SaveAll" == name ) {
+		icon = QIcon::fromTheme("document-save-all",
+				QIcon(":/icons/document-save-all.png"));
+	} else if ( "Print" == name ) {
+		icon = QIcon::fromTheme("document-print",
+				QIcon(":/icons/document-print.png"));
+	} else if ( "Undo" == name ) {
+		icon = QIcon::fromTheme("edit-undo",
+				QIcon(":/icons/edit-undo.png"));
+	} else if ( "Redo"== name ) {
+		icon = QIcon::fromTheme("edit-redo",
+				QIcon(":/icons/edit-redo.png"));
+	} else if ( "Cut" == name ) {
+		icon = QIcon::fromTheme("edit-cut",
+				QIcon(":/icons/edit-cut.png"));
+	} else if ( "Copy" == name ) {
+		icon = QIcon::fromTheme("edit-copy",
+				QIcon(":/icons/edit-copy.png"));
+	} else if ( "Paste" == name ) {
+		icon = QIcon::fromTheme("edit-paste",
+				QIcon(":/icons/edit-paste.png"));
+	} else if ( "Replace" == name ) {
+		icon = QIcon::fromTheme("edit-find-replace",
+				QIcon(":/icons/edit-find-replace.png"));
+	} else if ( "FindNext" == name ) {
+		icon = QIcon::fromTheme("go-next",
+				QIcon(":/icons/go-next.png"));
+	} else if ( "FindPrev" == name ) {
+		icon = QIcon::fromTheme("go-previous",
+				QIcon(":/icons/go-previous.png"));
+	} else if ( "LoadSesn" == name ) {
+		icon = QIcon::fromTheme("folder-open",
+				QIcon(":/icons/document-open-folder.png"));
+	} else if ( "SaveSesn" == name ) {
+		icon = QIcon::fromTheme("document-save-as",
+				QIcon(":/icons/document-save-as.png"));
+	} else if ( "RunScript" == name ) {
+		icon = QIcon::fromTheme("system-run",
+				QIcon(":/icons/system-run.png"));
+	} else if ( "Make" == name ) {
+		icon = QIcon::fromTheme("run-build", 
+				QIcon(":/icons/run-build.png"));
+	} else if ( "RunCtags" == name ) {
+		icon = QIcon(":/icons/table.png");
+	} else if ( "TagJump" == name ) {
+		icon = QIcon::fromTheme("go-jump",
+				QIcon(":/icons/go-jump.png"));
+	} else if ( "Help" == name ) {
+		icon = QIcon::fromTheme("help-contents",
+				QIcon(":/icons/help-contents.png"));
+	} else if ( "FindHelp" == name ) {
+		icon = QIcon::fromTheme("help-faq",
+				QIcon(":/icons/help-contextual.png"));
+	}
+
+	if ( icon.isNull() ) { // last resort
+		icon = QIcon::fromTheme(name.toLower());
+	}
+
+	return icon;
+}
+
+/**
+ * Get an icon for a given name
+ *
+ */
+QIcon VimWrapper::icon(const QString& name)
+{
+	QIcon icon = iconFromTheme(name);
+
+	if ( icon.isNull() ) {
+		return QApplication::style()->standardIcon(QStyle::SP_FileLinkIcon);
+	}
+
+	return icon;
+}
+
+
+QColor
+VimWrapper::fromColor(long color)
+{
+	if ( color == INVALCOLOR ) {
+		return QColor();
+	}
+
+	int red = ((color & 0x00FF0000) >> 16);
+	int green = ((color & 0x0000FF00) >>  8);
+	int blue = ((color & 0x000000FF) >>  0);
+
+	return QColor(red, green, blue);
+}
+
+long
+VimWrapper::toColor(const QColor& c)
+{
+	return ((long)c.red() << 16) + ((long)c.green() << 8) + ((long)c.blue());
+}
+
+void
+VimWrapper::newTab(int idx)
+{
+	send_tabline_menu_event(idx, TABLINE_MENU_NEW);
+}
+
+QByteArray VimWrapper::convertTo(const QString& s)
+{
+	bool m_encoding_utf8 = true; // FIXME: check encoding
+
+	if ( m_encoding_utf8 ) {
+		return s.toUtf8();
+	} else {
+		return s.toAscii();
+	}
+}
+
+QString VimWrapper::convertFrom(const char *s, int size)
+{
+	bool m_encoding_utf8 = true; // FIXME: check encoding
+
+	if ( m_encoding_utf8 ) {
+		return QString::fromUtf8(s, size);
+	} else {
+		return QString::fromAscii(s, size);
+	}
+}
+
+

src/Makefile
@@ -1241,11 +1241,13 @@ MOC		= $(QT_MOC)
 QT_LIBS1	= -L$(QT_LIBDIR) -lQtGui -lQtCore -lstdc++
 QT_SRC	= gui.c gui_qt.cpp pty.c gui_beval.c \
 			qt/qvimshell.cpp qt/mainwindow.cpp qt/vimaction.cpp qt/vimscrollbar.cpp \
-			qt/vimgui.cpp qt/fontdialog.cpp qt/scrollarea.cpp
+			qt/fontdialog.cpp qt/scrollarea.cpp \
+			qt/vimwrapper.cpp
 QT_OBJ	= objects/gui.o objects/gui_qt.o \
 			objects/pty.o objects/gui_beval.o \
 			objects/mainwindow.o objects/qvimshell.o objects/vimaction.o objects/qtresources.o \
-			objects/vimscrollbar.o objects/vimgui.o objects/fontdialog.o objects/scrollarea.o
+			objects/vimscrollbar.o objects/fontdialog.o objects/scrollarea.o objects/vimwrapper.o
+
 QT_DEFS	= -DFEAT_GUI_QT $(NARROW_PROTO) -Iqt -I. -I$(QT_INCDIR)
 QT_IPATH	= $(GUI_INC_LOC)
 QT_LIBS_DIR = $(GUI_LIB_LOC)
@@ -2552,6 +2554,10 @@ objects/fontdialog.o: qt/fontdialog.cpp
 objects/scrollarea.o: qt/scrollarea.cpp
 	$(MOC) qt/scrollarea.h > scrollarea.moc
 	$(CXX) $(ALL_CFLAGS) -o $@ -c qt/scrollarea.cpp
+
+objects/vimwrapper.o: qt/vimwrapper.cpp
+	$(MOC) qt/vimwrapper.h > vimwrapper.moc
+	$(CXX) $(ALL_CFLAGS) -o $@ -c qt/vimwrapper.cpp
 
 objects/gui_beval.o: gui_beval.c
 	$(CCC) -o $@ gui_beval.c

src/gui_qt.cpp
@@ -18,12 +18,6 @@ static QColor foregroundColor;
 static QColor backgroundColor;
 static QColor specialColor;
 
-//
-// A mutex guard against concurrent execution 
-// of QApplication::processEvents()
-//
-static QMutex loop_guard;
-
 /**
  * Raise application window
  */
@@ -123,9 +117,8 @@ gui_mch_flash(int msec)
 int
 gui_mch_wait_for_chars(long wtime)
 {
-	QMutexLocker l(&loop_guard);
 	if ( wtime == -1 ) {
-		QApplication::processEvents( QEventLoop::WaitForMoreEvents | QEventLoop::ExcludeSocketNotifiers);
+		QApplication::processEvents( QEventLoop::WaitForMoreEvents);
 		return OK;
 	} else {
 		//
@@ -140,12 +133,13 @@ gui_mch_wait_for_chars(long wtime)
 		QTime t;
 		t.start();
 		do {
-			QApplication::processEvents( QEventLoop::WaitForMoreEvents | QEventLoop::ExcludeSocketNotifiers);
+			QApplication::processEvents( QEventLoop::WaitForMoreEvents );
 			if ( vimshell->hasInput() ) {
 				return OK;
 			}
 		} while( t.elapsed() < wtime );
 	}
+
 	return FAIL;
 }
 
@@ -158,20 +152,8 @@ gui_mch_wait_for_chars(long wtime)
 void
 gui_mch_update()
 {
-	//
-	// We used to process Events here, however Qt
-	// cannot handle recursive calls to resizeEvent.
-	// The end result was:
-	//
-	// gui_mch_wait_for_chars -> resizeEvent -> gui_resize_shell-> (??)
-	// -> gui_mch_update -> (!crash!)
-	//
-	// Now we enforce that only one of (wait_for_chars/mch_update) may
-	// call process events at a time
-	//
-	if (loop_guard.tryLock()) {
+	if ( QApplication::hasPendingEvents() ) {
 		QApplication::processEvents();
-		loop_guard.unlock();
 	}
 }
 
@@ -193,7 +175,7 @@ void
 gui_mch_set_fg_color(guicolor_T	color)
 {
 	if ( color != INVALCOLOR ) {
-		foregroundColor = VimGui::fromColor(color);
+		foregroundColor = VimWrapper::fromColor(color);
 	}
 }
 
@@ -206,7 +188,7 @@ gui_mch_set_bg_color(guicolor_T color)
 	// The shell needs a hint background color
 	// to paint the back when resizing
 	if ( color != INVALCOLOR ) {
-		backgroundColor = VimGui::fromColor(color);
+		backgroundColor = VimWrapper::fromColor(color);
 	}
 }
 
@@ -252,7 +234,7 @@ gui_mch_clear_all()
 {
 	PaintOperation op;
 	op.type = CLEARALL;
-	op.color = VimGui::fromColor(gui.back_pixel);
+	op.color = VimWrapper::fromColor(gui.back_pixel);
 	vimshell->queuePaintOp(op);
 }
 
@@ -352,11 +334,11 @@ gui_mch_get_rgb(guicolor_T pixel)
 void
 gui_mch_clear_block(int row1, int col1, int row2, int col2)
 {
-	QRect rect = VimGui::mapBlock(row1, col1, row2, col2); 
+	QRect rect = VimWrapper::mapBlock(row1, col1, row2, col2); 
 
 	PaintOperation op;
 	op.type = FILLRECT;
-	op.color = VimGui::fromColor(gui.back_pixel);
+	op.color = VimWrapper::fromColor(gui.back_pixel);
 	op.rect = rect;
 	vimshell->queuePaintOp(op);
 }
@@ -380,7 +362,7 @@ clear_shell_border()
 
 	PaintOperation op;
 	op.type = FILLRECT;
-	op.color = VimGui::fromColor(gui.back_pixel);
+	op.color = VimWrapper::fromColor(gui.back_pixel);
 	op.rect = QRect(tl, br);
 	vimshell->queuePaintOp(op);
 }
@@ -392,14 +374,14 @@ clear_shell_border()
 void
 gui_mch_insert_lines(int row, int num_lines)
 {
-	QRect scrollRect = VimGui::mapBlock(row, gui.scroll_region_left, 
+	QRect scrollRect = VimWrapper::mapBlock(row, gui.scroll_region_left, 
 					gui.scroll_region_bot, gui.scroll_region_right);
 
 	PaintOperation op1;
 	op1.type = SCROLLRECT;
 	op1.rect = scrollRect;
 	op1.pos = QPoint(0, num_lines*gui.char_height);
-	op1.color = VimGui::fromColor(gui.back_pixel);
+	op1.color = VimWrapper::fromColor(gui.back_pixel);
 	vimshell->queuePaintOp(op1);
 
 	clear_shell_border();
@@ -413,14 +395,14 @@ void
 gui_mch_delete_lines(int row, int num_lines)
 {
 	// This used to be Bottom+1 and right+1
-	QRect scrollRect = VimGui::mapBlock(row, gui.scroll_region_left, 
+	QRect scrollRect = VimWrapper::mapBlock(row, gui.scroll_region_left, 
 					gui.scroll_region_bot, gui.scroll_region_right);
 
 	PaintOperation op;
 	op.type = SCROLLRECT;
 	op.rect = scrollRect;
 	op.pos = QPoint(0, -num_lines*gui.char_height);
-	op.color = VimGui::fromColor(gui.back_pixel);
+	op.color = VimWrapper::fromColor(gui.back_pixel);
 	vimshell->queuePaintOp(op);
 }
 
@@ -480,22 +462,22 @@ gui_mch_init()
 	display_errors();
 
 	/* Colors */
-	gui.norm_pixel = VimGui::toColor(QColor(Qt::black));
-	gui.back_pixel = VimGui::toColor(QColor(Qt::white));
+	gui.norm_pixel = VimWrapper::toColor(QColor(Qt::black));
+	gui.back_pixel = VimWrapper::toColor(QColor(Qt::white));
 
 	set_normal_colors();
 	gui_check_colors();
 	highlight_gui_started();
 
-	gui.def_norm_pixel = VimGui::toColor(QColor(Qt::black));
-	gui.def_back_pixel = VimGui::toColor(QColor(Qt::white));
+	gui.def_norm_pixel = VimWrapper::toColor(QColor(Qt::black));
+	gui.def_back_pixel = VimWrapper::toColor(QColor(Qt::white));
 
 	// The Scrollbar manages the scrollbars
 	gui.scrollbar_width = 0;
 	gui.scrollbar_height = 0;
 
 	// Background color hint
-	vimshell->setBackground(VimGui::backgroundColor() );
+	vimshell->setBackground(VimWrapper::backgroundColor() );
 
 	return OK;
 }
@@ -541,7 +523,6 @@ gui_mch_set_shellsize(int width, int height, int min_width, int min_height,
 	// call Qt to do it for us.
 	//
 	gui_resize_shell(vimshell->size().width(), vimshell->size().height());
-	gui_mch_update();
 }
 
 /**
@@ -570,7 +551,7 @@ void
 gui_mch_settitle(char_u *title, char_u *icon)
 {
 	if ( title != NULL ) {
-		window->setWindowTitle( vimshell->convertFrom((char*)title) );
+		window->setWindowTitle( VimWrapper::convertFrom((char*)title) );
 	}
 }
 
@@ -636,7 +617,7 @@ gui_mch_iconify()
 void
 gui_mch_invert_rectangle(int row, int col, int nr, int nc)
 {
-	QRect rect = VimGui::mapBlock(row, col, row+nr-1, col +nc-1);
+	QRect rect = VimWrapper::mapBlock(row, col, row+nr-1, col +nc-1);
 
 	PaintOperation op;
 	op.type = INVERTRECT;
@@ -722,7 +703,7 @@ clip_mch_set_selection(VimClipboard *cbd)
 
 	if (type >= 0) {
 		QClipboard *clip = QApplication::clipboard();
-		clip->setText( vimshell->convertFrom((char *)str, size), (QClipboard::Mode)cbd->clipboardMode);
+		clip->setText( VimWrapper::convertFrom((char *)str, size), (QClipboard::Mode)cbd->clipboardMode);
 	}
 
 	vim_free(str);
@@ -737,7 +718,7 @@ clip_mch_request_selection(VimClipboard *cbd)
 {
 
 	QClipboard *clip = QApplication::clipboard();
-	QByteArray text = vimshell->convertTo(clip->text( (QClipboard::Mode)cbd->clipboardMode));
+	QByteArray text = VimWrapper::convertTo(clip->text( (QClipboard::Mode)cbd->clipboardMode));
 
 	char_u	*buffer;
 	buffer = lalloc( text.size(), TRUE);
@@ -784,7 +765,7 @@ gui_mch_draw_hollow_cursor(guicolor_T color)
 	PaintOperation op;
 	op.type = DRAWRECT;
 	op.rect = rect;
-	op.color = VimGui::fromColor(color);
+	op.color = VimWrapper::fromColor(color);
 	vimshell->queuePaintOp(op);
 }
 
@@ -813,7 +794,7 @@ gui_mch_draw_part_cursor(int w, int h, guicolor_T color)
 	PaintOperation op;
 	op.type = FILLRECT;
 	op.rect = rect;
-	op.color = VimGui::fromColor(color);
+	op.color = VimWrapper::fromColor(color);
 	vimshell->queuePaintOp(op);
 }
 
@@ -824,7 +805,7 @@ void
 gui_mch_set_sp_color(guicolor_T color) 
 {
 	if ( color != INVALCOLOR ) {
-		specialColor = VimGui::fromColor(color);
+		specialColor = VimWrapper::fromColor(color);
 	}
 }
 
@@ -839,7 +820,7 @@ gui_mch_draw_string(
     int		len,
     int		flags)
 {
-	QString str = vimshell->convertFrom((char *)s, len);
+	QString str = VimWrapper::convertFrom((char *)s, len);
 	
 	// Font
 	QFont f = vimshell->font();
@@ -849,8 +830,8 @@ gui_mch_draw_string(
 
 	QFontMetrics fm(f);
 
-	int cellwidth = VimGui::stringCellWidth(str);
-	QPoint pos = VimGui::mapText(row, col);
+	int cellwidth = VimWrapper::stringCellWidth(str);
+	QPoint pos = VimWrapper::mapText(row, col);
 	QRect rect( pos.x(), pos.y(), gui.char_width*cellwidth, gui.char_height);
 
 	if (flags & DRAW_TRANSP) {
@@ -891,7 +872,7 @@ gui_mch_get_color(char_u *reqname)
 	}
 	QColor c = vimshell->color((char*)reqname);
 	if ( c.isValid() ) {
-		return VimGui::toColor(c);
+		return VimWrapper::toColor(c);
 	}
 
 	return INVALCOLOR;
@@ -995,7 +976,6 @@ gui_mch_toggle_tearoffs(int enable)
 void
 gui_mch_draw_menubar()
 {
-	gui_mch_update();
 }
 
 /**
@@ -1061,15 +1041,15 @@ gui_mch_add_menu(vimmenu_T *menu, int idx)
 	menu->qmenu = NULL;
 
 	if ( menu_is_popup(menu->name) ) {
-		menu->qmenu = new QMenu(vimshell->convertFrom((char*)menu->name), vimshell);
+		menu->qmenu = new QMenu(VimWrapper::convertFrom((char*)menu->name), vimshell);
 		return;
 	} else if ( menu_is_toolbar(menu->name) ) {
 		menu->qmenu = window->toolBar();
 	} else if ( menu->parent == NULL ) {
-		menu->qmenu = window->menuBar()->addMenu( vimshell->convertFrom((char*)menu->name) );
+		menu->qmenu = window->menuBar()->addMenu( VimWrapper::convertFrom((char*)menu->name) );
 	} else if ( menu->parent && menu->parent->qmenu ) {
 		QMenu *m = (QMenu*)menu->parent->qmenu;
-		menu->qmenu = m->addMenu( vimshell->convertFrom((char*)menu->name) );
+		menu->qmenu = m->addMenu( VimWrapper::convertFrom((char*)menu->name) );
 	}
 }
 
@@ -1168,8 +1148,8 @@ void gui_mch_show_popupmenu(vimmenu_T *menu)
 void
 gui_mch_def_colors()
 {
-	gui.norm_pixel = VimGui::toColor(QColor(Qt::black));
-	gui.back_pixel = VimGui::toColor(QColor(Qt::white));
+	gui.norm_pixel = VimWrapper::toColor(QColor(Qt::black));
+	gui.back_pixel = VimWrapper::toColor(QColor(Qt::white));
 	gui.def_norm_pixel = gui.norm_pixel;
 	gui.def_back_pixel = gui.back_pixel;
 }
@@ -1294,7 +1274,7 @@ gui_mch_browse(int saving, char_u *title, char_u *dflt, char_u *ext, char_u *ini
 		return NULL;
 	}
 
-	return vim_strsave((char_u *) vimshell->convertTo(file).data());
+	return vim_strsave((char_u *) VimWrapper::convertTo(file).data());
 }
 
 
@@ -1337,7 +1317,7 @@ gui_mch_dialog(int type, char_u *title, char_u *message, char_u *buttons, int df
 	if ( buttons != NULL ) {
 		QStringList b_string;
 
-		b_string = vimshell->convertFrom((char*)buttons).split(DLG_BUTTON_SEP);
+		b_string = VimWrapper::convertFrom((char*)buttons).split(DLG_BUTTON_SEP);
 
 		QListIterator<QString> it(b_string);
 		int bt=1;
@@ -1381,7 +1361,6 @@ void
 gui_mch_show_tabline(int showit)
 {
 	window->showTabline(showit != 0);
-	gui_mch_update();
 }
 
 /**
@@ -1415,7 +1394,7 @@ gui_mch_update_tabline(void)
 
 		get_tabline_label(tp, FALSE);
 		char_u *labeltext = CONVERT_TO_UTF8(NameBuff);
-		window->setTab( nr, vimshell->convertFrom((char*)labeltext));
+		window->setTab( nr, VimWrapper::convertFrom((char*)labeltext));
 		CONVERT_TO_UTF8_FREE(labeltext);
 	}
 	window->removeTabs(nr);
@@ -1448,7 +1427,7 @@ gui_mch_font_dialog(char_u *oldval)
 
 	if ( dialog->exec() == QDialog::Accepted ) {
 		QFont f =  dialog->selectedFont();
-		QByteArray text = vimshell->convertTo( QString("%1 %2").arg(f.family()).arg(f.pointSize()) );
+		QByteArray text = VimWrapper::convertTo( QString("%1 %2").arg(f.family()).arg(f.pointSize()) );
 
 		char_u *buffer;
 		buffer = lalloc( text.size(), TRUE);

src/qt/mainwindow.cpp
@@ -41,7 +41,7 @@ MainWindow::MainWindow( gui_T* gui, QWidget *parent)
 	tabbar->setDrawBase(false);
 	tabbar->addTab("VIM"); // One tab must always exist
 
-	QAction *newTab = tabtoolbar->addAction( VimGui::icon("tab-new"), "New Tab");
+	QAction *newTab = tabtoolbar->addAction( VimWrapper::icon("tab-new"), "New Tab");
 	tabtoolbar->addWidget(tabbar);
 
 	connect( tabbar, SIGNAL(tabCloseRequested(int)),
@@ -175,7 +175,7 @@ void MainWindow::openNewTab()
 {
 	// The amusing +1 trick opens the
 	// tab at the right of other tabs
-	VimGui::newTab(tabbar->count()+1);
+	VimWrapper::newTab(tabbar->count()+1);
 	vimshell->forceInput();
 }
 

src/qt/qvimshell.cpp
@@ -32,13 +32,13 @@ void QVimShell::setBackground(const QColor color)
 
 void QVimShell::switchTab(int idx)
 {
-	send_tabline_event(idx);
+	vim.sendTablineEvent(idx);
 	m_input = true;
 }
 
 void QVimShell::closeTab(int idx)
 {
-	send_tabline_menu_event(idx, TABLINE_MENU_CLOSE);
+	vim.sendTablineMenuEvent(idx, TABLINE_MENU_CLOSE);
 	m_input = true;
 }
 
@@ -67,7 +67,7 @@ void QVimShell::resizeEvent(QResizeEvent *ev)
 	}
 
 	update();
-	gui_resize_shell(ev->size().width(), ev->size().height());
+	vim.guiResizeShell(ev->size().width(), ev->size().height());
 }
 
 /**
@@ -103,25 +103,6 @@ bool QVimShell::specialKey(QKeyEvent *ev, char* str, int *len)
 	}
 	return false;
 }
-
-QByteArray QVimShell::convertTo(const QString& s)
-{
-	if ( m_encoding_utf8 ) {
-		return s.toUtf8();
-	} else {
-		return s.toAscii();
-	}
-}
-
-QString QVimShell::convertFrom(const char *s, int size)
-{
-	if ( m_encoding_utf8 ) {
-		return QString::fromUtf8(s, size);
-	} else {
-		return QString::fromAscii(s, size);
-	}
-}
-
 
 bool QVimShell::hasInput()
 {
@@ -182,14 +163,14 @@ void QVimShell::keyPressEvent ( QKeyEvent *ev)
 		add_to_input_buf((char_u *) str, len);
 		return;
 	} else if ( !ev->text().isEmpty() ) {
-		add_to_input_buf( (char_u *) convertTo(ev->text()).data(), ev->count() );
+		add_to_input_buf( (char_u *) VimWrapper::convertTo(ev->text()).data(), ev->count() );
 		return;
 	}
 }
 
 void QVimShell::close()
 {
-	gui_shell_closed();
+	vim.guiShellClosed();
 }
 
 void QVimShell::closeEvent(QCloseEvent *event)
@@ -201,7 +182,7 @@ void QVimShell::closeEvent(QCloseEvent *event)
 bool QVimShell::isFakeMonospace(const QFont& f)
 {
 	QFontMetrics fm(f);
-	return ( fm.averageCharWidth() != VimGui::charWidth() );
+	return ( fm.averageCharWidth() != VimWrapper::charWidth() );
 }
 
 /*
@@ -270,12 +251,12 @@ void QVimShell::drawStringSlow( const PaintOperation& op, QPainter &painter )
 
 	QRect rect = op.rect;
 	foreach(QChar c, op.str) {
-		if ( VimGui::charCellWidth(c) == 1 ) {
-			rect.setWidth(VimGui::charWidth());
-		} else if (VimGui::charCellWidth(c) == 2) {
-			rect.setWidth(2*VimGui::charWidth());
+		if ( VimWrapper::charCellWidth(c) == 1 ) {
+			rect.setWidth(VimWrapper::charWidth());
+		} else if (VimWrapper::charCellWidth(c) == 2) {
+			rect.setWidth(2*VimWrapper::charWidth());
 		} else {
-			qDebug() << __func__ << "invalid lenght" << c << VimGui::charCellWidth(c);
+			qDebug() << __func__ << "invalid lenght" << c << VimWrapper::charCellWidth(c);
 			continue;
 		}
 		painter.drawText(rect, 
@@ -345,7 +326,7 @@ void QVimShell::flushPaintOps()
 			painter.drawRect(op.rect);
 			break;
 		case DRAWSTRING:
-			if ( getenv("QVIM_DRAW_STRING_SLOW") || op.str.length() != VimGui::stringCellWidth(op.str) ) {
+			if ( getenv("QVIM_DRAW_STRING_SLOW") || op.str.length() != VimWrapper::stringCellWidth(op.str) ) {
 				drawStringSlow(op, painter);
 			} else if ( isFakeMonospace(op.font) ) {
 				qDebug() << __func__ << "Font size mismatch a.k.a. this is not a proper monospace font";
@@ -395,10 +376,10 @@ void QVimShell::paintEvent ( QPaintEvent *ev )
 void QVimShell::mouseMoveEvent(QMouseEvent *ev)
 {	
 	if ( ev->buttons() ) {
-		gui_send_mouse_event(MOUSE_DRAG, ev->pos().x(),
+		vim.guiSendMouseEvent(MOUSE_DRAG, ev->pos().x(),
 					  ev->pos().y(), FALSE, 0);
 	} else {
-		gui_mouse_moved(ev->pos().x(), ev->pos().y());
+		vim.guiMouseMoved(ev->pos().x(), ev->pos().y());
 	}
 	m_input = true;
 }
@@ -435,14 +416,14 @@ void QVimShell::mousePressEvent(QMouseEvent *ev)
 
 	int_u vmod = vimMouseModifiers(QApplication::keyboardModifiers());
 
-	gui_send_mouse_event(but, ev->pos().x(),
+	vim.guiSendMouseEvent(but, ev->pos().x(),
 					  ev->pos().y(), repeat, vmod);
 	m_input = true;
 }
 
 void QVimShell::mouseReleaseEvent(QMouseEvent *ev)
 {
-	gui_send_mouse_event(MOUSE_RELEASE, ev->pos().x(),
+	vim.guiSendMouseEvent(MOUSE_RELEASE, ev->pos().x(),
 					  ev->pos().y(), FALSE, 0);
 	m_input = true;
 	setMouseTracking(false);
@@ -450,7 +431,7 @@ void QVimShell::mouseReleaseEvent(QMouseEvent *ev)
 
 void QVimShell::wheelEvent(QWheelEvent *ev)
 {
-	gui_send_mouse_event((ev->delta() > 0) ? MOUSE_4 : MOUSE_5,
+	vim.guiSendMouseEvent((ev->delta() > 0) ? MOUSE_4 : MOUSE_5,
 					    ev->pos().x(), ev->pos().y(), FALSE, 0);
 	m_input = true;
 }
@@ -467,7 +448,7 @@ void QVimShell::loadColors(const QString& name)
 	}
 
 	while (!f.atEnd()) {
-		QString line = convertFrom( f.readLine() );
+		QString line = VimWrapper::convertFrom( f.readLine() );
 		if ( line.startsWith("!") ) {
 			continue;
 		}
@@ -545,24 +526,10 @@ void QVimShell::dropEvent(QDropEvent *ev)
 		if ( urls.size() == 0 ) {
 			return;
 		}
+		vim.guiHandleDrop(ev->pos().x(), ev->pos().y(), 0, urls);
 
-		char_u **fnames = (char_u**)alloc( urls.size() * sizeof(char_u*));
-		int i;
-		for (i=0; i<urls.size(); i++) {
-			QByteArray encoded = convertTo(urls.at(i).toString());
-			char *s = (char*)alloc(encoded.size()*sizeof(char)+1);
-			int j;
-			for (j=0; j<encoded.size(); j++) {
-				s[j] = encoded.at(j);
-			}
-			s[j]='\0';
-
-			fnames[i] = (char_u *) s;
-		}
-		
-		gui_handle_drop(ev->pos().x(), ev->pos().y(), 0, fnames, urls.size());
 	} else {
-		QByteArray text = convertTo(ev->mimeData()->text());
+		QByteArray text = VimWrapper::convertTo(ev->mimeData()->text());
 		dnd_yank_drag_data( (char_u*)text.data(), text.size());
 
 		char_u buf[3] = {CSI, KS_EXTRA, (char_u)KE_DROP};
@@ -573,14 +540,14 @@ void QVimShell::dropEvent(QDropEvent *ev)
 
 void QVimShell::focusInEvent(QFocusEvent *ev)
 {
-	gui_focus_change(TRUE);
+	vim.guiFocusChanged(TRUE);
 	QWidget::focusInEvent(ev);
 	update();
 }
 
 void QVimShell::focusOutEvent(QFocusEvent *ev)
 {
-	gui_focus_change(FALSE);
+	vim.guiFocusChanged(FALSE);
 	QWidget::focusOutEvent(ev);
 	update();
 }
@@ -598,7 +565,7 @@ int QVimShell::charWidth()
 void QVimShell::inputMethodEvent(QInputMethodEvent *ev)
 {
 	if ( !ev->commitString().isEmpty() ) {
-		QByteArray s = convertTo(ev->commitString());
+		QByteArray s = VimWrapper::convertTo(ev->commitString());
 		add_to_input_buf( (char_u *) s.data(), s.size() );
 		tooltip("");
 	} else {
@@ -633,8 +600,8 @@ void QVimShell::tooltip(const QString& text)
 	}
 
 	if ( !m_tooltip->isVisible() ) {
-		m_tooltip->setMinimumHeight(VimGui::charHeight());
-		m_tooltip->move( VimGui::cursorPosition() );
+		m_tooltip->setMinimumHeight(VimWrapper::charHeight());
+		m_tooltip->move( VimWrapper::cursorPosition() );
 		m_tooltip->show();
 	}
 

src/qt/qvimshell.h
@@ -3,7 +3,7 @@
 
 #include <Qt/QtGui>
 #include "vimgui.h"
-
+#include "vimwrapper.h"
 
 
 
@@ -36,9 +36,6 @@ public:
 
 	QColor background();
 	int charWidth();
-
-	QByteArray convertTo(const QString& s);
-	QString convertFrom(const char *, int size=-1);
 
 	void setEncodingUtf8(bool);
 	static void setupColorMap();
@@ -105,6 +102,7 @@ private:
 	QLabel *m_tooltip;
 
 	static QHash<QString, QColor> m_colorMap;
+	VimWrapper vim;
 };
 
 struct special_key

src/qt/vimaction.cpp
@@ -4,7 +4,7 @@ VimAction::VimAction(vimmenu_T* menu, QObject *parent)
 :QAction(parent), m_menu(menu)
 {
 	if ( menu_is_toolbar(menu->parent->name) ) {
-		setIcon( VimGui::icon(QString::fromUtf8((char*)menu->name)) );
+		setIcon( VimWrapper::icon(QString::fromUtf8((char*)menu->name)) );
 		setToolTip(QString::fromUtf8((char*) menu->strings[MENU_INDEX_TIP]));
 	} else {
 		setText( QString::fromUtf8((char*)menu->name) );
@@ -16,5 +16,4 @@ VimAction::VimAction(vimmenu_T* menu, QObject *parent)
 void VimAction::actionTriggered()
 {
 	gui_menu_cb(m_menu);
-	QApplication::processEvents();	
 }

commit 91352679c5e4ab7c194483c0449d28595e990951

Fix wrong close tab event
- 5a101ab0 Introduced a wrong call intto send_tabline_menu_event
  where when the close tab button was pressed, the first tab would
  be closed regardless of the pressed tab button.

src/qt/vimwrapper.cpp
@@ -94,7 +94,7 @@ void VimWrapper::sendTablineMenuEvent(int idx, int ev)
 {
 	QMetaObject::invokeMethod(this, 
 				"slot_sendTablineMenuEvent", 
-				Qt::QueuedConnection, Q_ARG(int, ev),
+				Qt::QueuedConnection, Q_ARG(int, idx),
 							Q_ARG(int, ev));
 }
 void VimWrapper::slot_sendTablineMenuEvent(int idx, int ev)